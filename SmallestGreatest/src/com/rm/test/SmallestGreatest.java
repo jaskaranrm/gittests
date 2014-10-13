4bed4cfae3dd6a1180bc5cb98ab4e3b2dae3aaac
Jaskaran Singh <jaskaran@raremile.com>
2014-10-13 11:29:00 -0700

    Initial

---
+++ SmallestGreatest/src/com/rm/test/SmallestGreatest.java
@@ -0,0 +1,65 @@
+package com.rm.test;
+
+public class SmallestGreatest {
+
+	public static void main(String[] args) {
+
+		int N = 93337799;
+		int n1 = getMultipliedSingleDigit(N);
+		int n2 = getAddedSingleDigit(N);
+		int smallestGreatestNum = -1;
+
+		System.out.println("n1 "+n1);
+		System.out.println("n2 "+n2);
+		
+		for (int i = N + 1; i < 1000000000; i++) {
+			//System.out.println(i);
+			int multipliedSingleDigit = getMultipliedSingleDigit(i);
+			int addedSingleDigit = getAddedSingleDigit(i);
+			if (multipliedSingleDigit < n1 && addedSingleDigit < n2) {
+				smallestGreatestNum = i;
+				break;
+			}
+		}
+		System.out.println("Next smallest Greatest  number is "
+				+ smallestGreatestNum);
+	}
+
+	private static int getAddedSingleDigit(int n2) {
+		int noOfSteps = 0;
+		while (n2 > 9) {
+			n2 = addDigits(n2);
+			noOfSteps++;
+		}
+		return noOfSteps;
+	}
+
+	private static int getMultipliedSingleDigit(int n1) {
+		int noOfSteps = 0;
+		while (n1 > 9) {
+			n1 = multiplyDigits(n1);
+			noOfSteps++;
+		}
+		return noOfSteps;
+	}
+
+	private static int addDigits(int copyN) {
+		int sum = 0;
+		while (copyN > 0) {
+			int digit = copyN % 10;
+			sum += digit;
+			copyN /= 10;
+		}
+		return sum;
+	}
+
+	private static int multiplyDigits(int copyN) {
+		int product = 1;
+		while (copyN > 0) {
+			int digit = copyN % 10;
+			product *= digit;
+			copyN /= 10;
+		}
+		return product;
+	}
+}
